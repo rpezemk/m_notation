7e02c60ab411dd62b5b52345673a44c29bc0bcf3
rpezemk <rpezemk@gmail.com>
2024-11-20 01:21:34 +0100

    cosmetics

--- widgets/main_window.py
+++ widgets/main_window.py
@@ -1,8 +1,9 @@
 from PyQt5.QtWidgets import QMainWindow, QHBoxLayout, QVBoxLayout, QWidget, QPushButton
+from PyQt5.QtGui import QFont
+
 from model.piece import Piece
 import model.piece
 from widgets.note_widget import PartWidget
-
 import widgets.widget_utils as w_utils
 
 
@@ -10,20 +11,23 @@ import widgets.widget_utils as w_utils
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Stacked Panels")
         self.part_widgets = []
+        self.setWindowTitle("Stacked Panels")
         self.setStyleSheet("background-color: black;")
         
         all_widget = QWidget()
         all_layout = QVBoxLayout(all_widget)
-        
         top_button = QPushButton("top button")
+        top_button.setStyleSheet("color: white;")
+        font = QFont("Courier New") 
+        font.setStyleHint(QFont.Monospace)  
+        top_button.setFont(font)
         top_button.clicked.connect(self.button_click)
         top_button.setFixedHeight(30)
-        all_layout.addWidget(top_button)
         
         pane_and_scores_widget = QWidget()
         pane_and_scores_widget.setStyleSheet("background-color: black;")
+        
         pane_and_scores_layout =  QHBoxLayout(pane_and_scores_widget)
         pane_and_scores_layout.setSpacing(0)
         pane_and_scores_layout.setContentsMargins(0, 0, 0, 0)
@@ -33,12 +37,12 @@ class MainWindow(QMainWindow):
         
         scores_widget = QWidget()
         self.part_layouts = QVBoxLayout(scores_widget)
-                    
         self.part_layouts.addStretch()
-        
+                    
         pane_and_scores_layout.addWidget(left_pane_widget)
         pane_and_scores_layout.addWidget(scores_widget)
         
+        all_layout.addWidget(top_button)
         all_layout.addWidget(pane_and_scores_widget)        
         self.setCentralWidget(all_widget)
     
@@ -47,10 +51,9 @@ class MainWindow(QMainWindow):
         self.load_piece(piece)
         
     def resizeEvent(self, event):
-        new_size = event.size()  
-        self.setWindowTitle(f"Window resized to: {new_size.width()} x {new_size.height()}")
+        size = event.size()  
+        self.setWindowTitle(f"Window resized to: {size.width()} x {size.height()}")
         super().resizeEvent(event)  
-        self.show()
     
     def load_piece(self, piece: Piece):
         w_utils.clear_layout(self.part_layouts)
--- widgets/note_widget.py
+++ widgets/note_widget.py
@@ -15,23 +15,24 @@ class PartWidget(QWidget):
         
         self.left_area_width = 100  # Fixed width for the left area
         layout = QHBoxLayout(self)
+        self.setStyleSheet("background-color: lightblue; border: 1px solid black;")
         layout.setContentsMargins(0, 0, 0, 0)
         layout.setSpacing(0)
         
         # Left area with a label
-        self.left_area = QWidget()
-        self.left_area.setFixedWidth(self.left_area_width)
-        self.left_area.setStyleSheet("background-color: lightgray; border-right: 1px solid black;")
-        self.label = QLabel("Label", self.left_area)
-        self.label.setAlignment(Qt.AlignCenter)
-        left_layout = QVBoxLayout(self.left_area)
+        left_area = QWidget()
+        left_area.setFixedWidth(self.left_area_width)
+        left_area.setStyleSheet("background-color: lightgray; border-right: 1px solid black;")
+        label = QLabel("Label", left_area)
+        label.setAlignment(Qt.AlignCenter)
+        left_layout = QVBoxLayout(left_area)
         left_layout.setContentsMargins(0, 0, 0, 0)
-        left_layout.addWidget(self.label)
+        left_layout.addWidget(label)
+        
         # Right area with StaffWidget
         self.staff_widget = StaffWidget(foreground=99)
-        layout.addWidget(self.left_area)
+        layout.addWidget(left_area)
         layout.addWidget(self.staff_widget)
-        self.setStyleSheet("background-color: lightblue; border: 1px solid black;")
 
 class StaffWidget(QWidget):
     def __init__(self, foreground: int = 99):
@@ -54,59 +55,43 @@ class StaffWidget(QWidget):
         self.bar_left_margin = 15
         self.bar_right_margin = 5
         self.visual_notes = []
-
-    def get_no_of_measures(self):
-        return self.no_of_measures
-    
+        self.y_offsets = None
+        
     def set_bars(self, measures: list[Measure]):
         self.measures = measures
-
-
         
     def paintEvent(self, event):
-        w = self.width()
-        h = self.height()
-        if w < 30 or h < 30:
+        if self.width() < 30 or self.height() < 30:
             return
+        self.draw_content()
         
+    def draw_content(self):
         painter = QPainter(self)
-        painter.setRenderHint(QPainter.Antialiasing, False)
+        self.y_offsets = self.get_y_offsets()
         painter.setFont(self.bravura_font)
-        
+        painter.setPen(self.dark_gray)
+        painter.setBrush(self.dark_gray)
         self.draw_clef(painter) 
         self.draw_staff_lines(painter)
         self.draw_bar_lines(painter)
         self.calculate_vis_notes(painter)
+        painter.setPen(self.light_gray)
+        painter.setBrush(self.light_gray)
         self.draw_visual_notes(painter)
-        
         painter.end()
 
-    def draw_clef(self, painter):
-        painter.setPen(self.dark_gray)
-        painter.setBrush(self.dark_gray)
-        text_rect = QRect(0, -23, 40, 200)
-        painter.drawText(text_rect, Qt.AlignTop, Glyphs.G_Clef)
+    def draw_clef(self, painter: QPainter):
+        painter.drawText(QRect(0, -23, 40, 200), Qt.AlignTop, Glyphs.G_Clef)
 
-    def get_bar_line_offsets(self) -> list[int]:
-        av_space = self.width() - self.clef_margin
-        if av_space < 10:
-            return [0, 10]
-        self.measure_width = int(av_space/self.no_of_measures)
-        infos = []
-        for i in range(0, self.no_of_measures + 1):
-            infos.append(self.clef_margin + self.measure_width * i)
-        return infos
-
-    def get_inner_bar_segments(self):
+    def get_y_segments(self):
         l_mar = self.bar_left_margin
         r_mar = self.bar_right_margin
-        infos = self.get_bar_line_offsets()
-        areas = [infos[idx:idx+2] for idx in range(0, len(infos)-1)]
-        areas = [[a[0] + l_mar, a[1] - r_mar] for a in areas]
-        return areas
+        areas = [self.y_offsets[idx:idx+2] for idx in range(0, len(self.y_offsets)-1)]
+        areas_2 = [[a[0] + l_mar, a[1] - r_mar] for a in areas]
+        return areas_2
         
-    def calculate_vis_notes(self, painter):
-        bar_segments = self.get_inner_bar_segments()
+    def calculate_vis_notes(self, painter: QPainter):
+        bar_segments = self.get_y_segments()
         self.visual_notes = []
         for m_no, measure in enumerate(self.measures):
             seg_start = bar_segments[m_no][0]
@@ -130,39 +115,29 @@ class StaffWidget(QWidget):
                     vis_note = VisualNote(note, (x_0, res_y))
                     self.visual_notes.append(vis_note)
                     
-    def draw_visual_notes(self, painter):
+    def draw_visual_notes(self, painter: QPainter):
         for v_n in self.visual_notes:
             if isinstance(v_n.inner_note, Note):
                 self.draw_note(painter, v_n)                
             if isinstance(v_n.inner_note, Rest):                
                 self.draw_rest(painter, v_n)
                 
-    def get_staff_line_infos(self):
-        offsets = []
-        for i in range(0, 5):
-            offsets.append(self.staff_offset + i*self.line_spacing)
-        return offsets
-        
-    def draw_staff_lines(self, painter):
-        painter.setBrush(self.dark_gray)
-        painter.setPen(self.dark_gray)
+    def draw_staff_lines(self, painter: QPainter):
         for y_offset in self.get_staff_line_infos():
             painter.drawRect(QRect(0, y_offset, self.width(), 1))
 
     def draw_bar_lines(self, painter):
         self.measure_width = int(self.width()/self.no_of_measures)
         painter.drawRect(QRect(0, self.staff_offset, 1, 4*self.line_spacing))
-        for s in self.get_bar_line_offsets()[1:]:
+        for s in self.y_offsets[1:]:
             curr_x = s
             painter.drawRect(QRect(curr_x, self.staff_offset, 1, 4*self.line_spacing))
                     
-    def draw_note(self, painter, vis_note: VisualNote):
-        painter.setPen(self.light_gray)
-        text_rect = QRect(vis_note.point[0] - self.note_size, vis_note.point[1] - self.note_size, self.note_size * 2, self.note_size * 2)
-        painter.drawText(text_rect, Qt.AlignCenter, Glyphs.EighthNote) 
+    def draw_note(self, painter: QPainter, vis_note: VisualNote):
+        q_rect_args = vis_note.point[0] - self.note_size, vis_note.point[1] - self.note_size, self.note_size * 2, self.note_size * 2
+        painter.drawText(QRect(*q_rect_args), Qt.AlignCenter, Glyphs.EighthNote) 
 
     def draw_rest(self, painter, vis_note: VisualNote):
-        painter.setPen(self.light_gray)
         text_rect = QRect(vis_note.point[0] - self.note_size, vis_note.point[1] - self.note_size, self.note_size * 2, self.note_size * 2)
         painter.drawText(text_rect, Qt.AlignCenter, Glyphs.Rest_Eighth) 
 
@@ -183,3 +158,20 @@ class StaffWidget(QWidget):
                     self.visual_notes[idx] = new_vis_note
                     self.update()  
                     break  
+
+    def get_y_offsets(self) -> list[int]:
+        av_space = self.width() - self.clef_margin
+        if av_space < 10:
+            return [0, 10]
+        self.measure_width = int(av_space/self.no_of_measures)
+        infos = []
+        for i in range(0, self.no_of_measures + 1):
+            infos.append(self.clef_margin + self.measure_width * i)
+        return infos
+    
+    def get_staff_line_infos(self):
+        offsets = []
+        for i in range(0, 5):
+            offsets.append(self.staff_offset + i*self.line_spacing)
+        return offsets
+        
