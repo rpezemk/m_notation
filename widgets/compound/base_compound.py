9b57c1e077677994c118ebf7d9403b77a78a9662
rpezemk <rpezemk@gmail.com>
2024-11-28 00:19:29 +0100

    HStack/VStack/MyCompound changes

--- widgets/compound/base_compound.py
+++ widgets/compound/base_compound.py
@@ -1,12 +1,30 @@
 from PyQt5.QtWidgets import QWidget
 
 class MyCompound():
-    def __init__(self, t: type):
+    def __init__(self, t: type, margin: tuple|list = None, spacing: int=None, children: list['MyCompound'] = None, stretch=False,
+                 black_on_white=False
+                 ):
         self.widget = QWidget()
         self.widget.setStyleSheet("background-color: black;")
         self.layout = t(self.widget)
+        if spacing is not None:
+            self.layout.setSpacing(spacing)
+        if children is not None:
+            for c in children:
+                if isinstance(c, MyCompound):
+                    self.layout.addWidget(c.widget)
+                elif isinstance(c, QWidget):
+                    self.layout.addWidget(c)
+        if stretch:
+            self.layout.addStretch()   
+        if margin is not None:
+            self.layout.setContentsMargins(*margin)
+        if black_on_white:
+            self.widget.setStyleSheet("background-color: lightgray; border-right: 1px solid black;")
+
         
     def add_widgets(self, widgets: list[QWidget]):
         for w in widgets:
             self.layout.addWidget(w)
         return self
+            
--- widgets/compound/stack_panels.py
+++ widgets/compound/stack_panels.py
@@ -4,49 +4,33 @@ from widgets.compound.base_compound import MyCompound
 
 
 class HStack(MyCompound):
-    def __init__(self, margin: tuple|list = None, spacing: int=None, children: list[MyCompound] = None):
-        super().__init__(QHBoxLayout)
-        if margin is not None:
-            self.set_content_margins(margin[0], margin[1], margin[2], margin[3])
-        if spacing is not None:
-            self.set_spacing(spacing)
-        if children is not None:
-            for c in children:
-                if isinstance(c, MyCompound):
-                    self.layout.addWidget(c.widget)
-                elif isinstance(c, QWidget):
-                    self.layout.addWidget(c)
-                    
-    def set_spacing(self, spacing: int):
-        self.layout.setSpacing(spacing)
-        return self
-    
-    def set_content_margins(self, l, t, r, b):
-        self.layout.setContentsMargins(l, t, r, b)
-        return self
-    
+    def __init__(self, 
+                 margin: tuple|list = None, 
+                 spacing: int = None,
+                 children: list[MyCompound] = None, 
+                 black_on_white = False, 
+                 stretch = False):
+        super().__init__(QHBoxLayout, 
+                         margin=margin, 
+                         spacing=spacing, 
+                         children=children, 
+                         stretch=stretch, 
+                         black_on_white=black_on_white)
+
 class VStack(MyCompound):
-    def __init__(self, margin: tuple|list = None, children: list[MyCompound] = None, black_on_white=False):
-        super().__init__(QVBoxLayout)
-        if margin is not None:
-            self.set_content_margins(margin[0], margin[1], margin[2], margin[3])
-        if children is not None:
-            for c in children:
-                if isinstance(c, MyCompound):
-                    self.layout.addWidget(c.widget)
-                elif isinstance(c, QWidget):
-                    self.layout.addWidget(c)
-        if black_on_white:
-            self.widget.setStyleSheet("background-color: lightgray; border-right: 1px solid black;")
+    def __init__(self, 
+                 margin: tuple|list = None, 
+                 spacing: int = None,
+                 children: list[MyCompound] = None, 
+                 black_on_white = False, 
+                 stretch = False,
+                 fixed_width=-1):
+        super().__init__(QVBoxLayout, 
+                         margin=margin, 
+                         spacing=spacing, 
+                         children=children, 
+                         stretch=stretch, 
+                         black_on_white=black_on_white)
+        if fixed_width >= 0:
+            self.widget.setFixedWidth(fixed_width)  
             
-    def fixed_width(self, width: int):
-        self.widget.setFixedWidth(width)  
-        return self
-    
-    def add_stretch(self):
-        self.layout.addStretch()  
-        return self
-    
-    def set_content_margins(self, l, t, r, b):
-        self.layout.setContentsMargins(l, t, r, b)
-        return self
