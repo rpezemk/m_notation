67a62181c68066d75b054df1856d79875eb78504
rpezemk <rpezemk@gmail.com>
2024-12-01 10:04:48 +0100

    m_notator clean-up

--- m_notator.py
+++ m_notator.py
@@ -1,12 +1,10 @@
 import sys
-import os
-import random
-from typing import Tuple
-import PyQt5.QtGui
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
+
 from widgets.main_window import MainWindow
 from widgets.note_widget import PartWidget
 import model.piece 
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
