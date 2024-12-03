84d7fd50fa5980a639604e5b7709fc98b589f4a2
rpezemk <rpezemk@gmail.com>
2024-12-03 02:17:56 +0100

    kbd autorepeat ignored

--- utils/kbd_resolver.py
+++ utils/kbd_resolver.py
@@ -1,37 +1,33 @@
-from typing import Any, Callable
+import time
+from typing import Any, Callable, Tuple
 from PyQt5.QtWidgets import QApplication 
 from PyQt5.QtGui import QKeyEvent
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QKeySequence
-
 my_set = [Qt.Key_Control, Qt.Key_Shift, Qt.Key_Alt]
 
 class KbdResolver():
     def __init__(self):
-        self.normal_keys = []
-        self.modifiers = None
-        
+        self.curr_keys = []
 
+    def is_machine_time(self, t1, t2):
+        diff = t1 - t2
+        return abs(diff) < 0.01
+    
     def try_resolve_new_kbd_press(self, event: QKeyEvent):
-        key = event.key()
-        if key in my_set:
-            print("from my_set")
-        if key not in self.normal_keys:
-            self.normal_keys.append(key)
-            return True, self.normal_keys
-        return False, []
+        key = event.key()  
+        if key not in self.curr_keys:
+            self.curr_keys.append(key)
+        if event.isAutoRepeat():
+            return False, self.curr_keys
+        return True, self.curr_keys
         
     def try_resolve_new_kbd_release(self, event: QKeyEvent):
         key = event.key()
-        if key in my_set:
-            print("from my_set")
-        self.modifiers = event.modifiers()
-        self.modifiers & Qt.ControlModifier
-        if key in self.normal_keys:
-            self.normal_keys.remove(key)
-            return True, self.normal_keys
-        return False, []
-        
-        
+        if key in self.curr_keys:
+            self.curr_keys.remove(key)
+        if event.isAutoRepeat():
+            return False, self.curr_keys
+        return True, self.curr_keys
 
-        # Check for specific combinations
+        
