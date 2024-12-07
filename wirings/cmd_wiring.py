7eca2384ee233b87399de4a040ed652fe96c9f33
rpezemk <rpezemk@gmail.com>
2024-12-07 06:50:48 +0100

    added some key-commands

--- wirings/cmd_wiring.py
+++ wirings/cmd_wiring.py
@@ -6,11 +6,14 @@ CTRL_T = SubCmd("CTRL_T", [Qt.Key_Control, Qt.Key_T])
 CTRL_X = SubCmd("CTRL_X", [Qt.Key_Control, Qt.Key_X])
 CTRL_Q = SubCmd("CTRL_Q", [Qt.Key_Control, Qt.Key_Q])
 CTRL_R = SubCmd("CTRL_R", [Qt.Key_Control, Qt.Key_R])
-
+SPACE = SubCmd("PLAY", [Qt.Key_Space])
+RETURN = SubCmd("PLAY", [Qt.Key_Return])
 
 my_wirings = [
     CompoundCommand("ALL", [A], lambda: print("A-ALL")),
-    CompoundCommand("RENAME", [CTRL_R, CTRL_R], lambda: print("DO_STH WORKING")),
-    CompoundCommand("DELETE", [CTRL_T, CTRL_X], lambda: print("DO_STH WORKING")),
-    CompoundCommand("CLOSE", [CTRL_T, CTRL_Q], lambda: print("DO_STH WORKING"))
+    CompoundCommand("RENAME", [CTRL_R, CTRL_R], lambda: print("RENAME")),
+    CompoundCommand("DELETE", [CTRL_T, CTRL_X], lambda: print("DELETE")),
+    CompoundCommand("CLOSE", [CTRL_T, CTRL_Q], lambda: print("CLOSE")),
+    CompoundCommand("SPACE", [SPACE], lambda: print("SPACE")),
+    CompoundCommand("RETURN", [RETURN], lambda: print("RETURN"))
 ]
