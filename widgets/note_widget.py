2ba43c9c3049ca407aaad0cd7e0ccfe2f292c0ff
rpezemk <rpezemk@gmail.com>
2024-12-08 02:17:54 +0100

    pen color/style

--- widgets/note_widget.py
+++ widgets/note_widget.py
@@ -88,20 +88,24 @@ class AudioWidget(LaneWidget):
         painter.setFont(self.bravura_font)
         painter.setPen(self.dark_gray)
         painter.setBrush(self.black)
-        
+        pen = QPen(self.very_light_gray)  # Set the pen color to black
+        painter.setPen(pen)
         self.draw_frame(painter)
         # super().paintEvent(event)
         self.draw_ranges(painter)
+        pen = QPen(QColor(255, 255, 255, 80))  # Set the pen color to black
+        painter.setPen(pen)
+        pen.setWidth(1)       # Set the stroke thickness to 5 pixels
+        self.draw_data(painter)
         painter.end()
         
     def draw_frame(self, painter: QPainter):
         w = self.width()
         h = self.height()
         rect = QRect(0, 0, w-1, h-1)
-        pen = QPen(self.very_light_gray)  # Set the pen color to black
-        pen.setWidth(2)       # Set the stroke thickness to 5 pixels
-        painter.setPen(pen)
         painter.drawRect(rect)
+    
+        
         
     def draw_ranges(self, painter: QPainter):
         w = self.width()
@@ -121,6 +125,35 @@ class AudioWidget(LaneWidget):
             rect = QRect(x0, y0, r_w, r_h)
             painter.drawRect(rect)
             
+            
+    def draw_data(self, painter: QPainter):
+        w = self.width()
+        h = self.height()
+        painter.setBrush(self.black)
+        
+        for rng in audio_ranges:
+            start = rng[0]    
+            end = rng[1]
+            time_range = self.view_time_end - self.view_time_start
+            
+            x0 = int(((start - self.view_time_start)/time_range) * w)
+            y0 = 5
+            r_w = int(((end - start)/time_range) * w)
+            r_h = h
+            self.draw_range_data(painter, x0, y0, r_w, r_h)
+            
+    def draw_range_data(self, painter: QPainter, x0, y0, r_w, r_h):
+        simplified = self.audiofile.get_simplified(r_h, 1)
+        max__ = max(simplified)
+        simplified = [(r*r_h)/max__ for r in simplified]
+        lenght = len(simplified)
+        cnt = 0
+    
+        for val in simplified:
+            res_x0 = int(x0 + (r_w*cnt/lenght))
+            val = int(val)
+            painter.drawPoint(res_x0, r_h - val - 5)
+            cnt += 1
             
 class StaffWidget(LaneWidget):
     def __init__(self):
