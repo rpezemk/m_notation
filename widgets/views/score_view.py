267a39548973656e8bcb3438a2ce653a9dec66ac
rpezemk <rpezemk@gmail.com>
2024-12-11 01:44:57 +0100

    coursor restored @ScoreView

--- widgets/views/score_view.py
+++ widgets/views/score_view.py
@@ -1,14 +1,16 @@
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QFrame, QWidget
+
 from model.piece import generate_sample_piece
 from widgets.compound.stack_panels import VStack
 from widgets.note_widget import PartWidget, StaffWidget
-
 
 class ScoreView(VStack):
     def __init__(self, margin = None, spacing = None, children = None, black_on_white=False, stretch=False, fixed_width=-1):
         super().__init__(margin, spacing, children, black_on_white, stretch, fixed_width)
         piece = generate_sample_piece(4, 8)
 
-        # self.back = QWidget(self.widget)
+        self.back = QWidget(self.widget)
 
         for part in piece.parts:
             part_widget = PartWidget(widget_type=StaffWidget)
@@ -19,29 +21,30 @@ class ScoreView(VStack):
         self.layout.addStretch()
         self.layout.parentWidget().update()
         self.layout.update()
-        self.delta = 2
-        # self.line_x0 = 100
-        # self.line_pos = self.line_x0
-        # self.timer = QTimer(self.widget)
-        # self.timer.timeout.connect(self.update_counter)  # Call update_counter every interval
-        # self.timer.start(100)  # Interval set to 1000ms (1 second)
-        # self.line = QFrame(self.back)
-        # self.widget.resizeEvent = self.resizeEvent
-        # self.draw_line(0)
-
-    # def draw_line(self, x0):
-    #     h = self.widget.height()
-    #     self.line.setFrameShape(QFrame.HLine)  # Horizontal line
-    #     self.line.setGeometry(QRect(x0, 0, 1, h))  # Set position and size
-    #     self.line.setStyleSheet("background-color: gray;")
+        self.delta = 1
+        self.line_x0 = 100
+        self.line_pos = self.line_x0
+        self.timer = QTimer(self.widget)
+        self.timer.timeout.connect(self.update_counter)  # Call update_counter every interval
+        self.timer.start(2)  # Interval set to 1000ms (1 second)
+        self.line = QFrame(self.back)
+        self.widget.resizeEvent = self.resizeEvent
         
-    # def update_counter(self):
-    #     w = self.widget.width()
-    #     self.line_pos = (self.line_pos + self.delta) % (w - self.line_x0)
-    #     self.draw_line(self.line_pos + self.line_x0)
-    
-    # def resizeEvent(self, event):
-    #     h = self.widget.height()
-    #     w = self.widget.width()
-    #     if w - 100 > 0:
-    #         self.back.setGeometry(0, 0, w, h)
+    def update_counter(self):
+        w = self.widget.width()
+        if w - self.line_x0 <= 0:
+            return
+        self.line_pos = (self.line_pos + self.delta) % (w - self.line_x0)
+        self.draw_line(self.line_pos + self.line_x0)
+        
+    def draw_line(self, x0):
+        h = self.widget.height()
+        self.line.setFrameShape(QFrame.HLine)  # Horizontal line
+        self.line.setGeometry(x0, 0, 1, h)  # Set position and size
+        self.line.setStyleSheet("background-color: gray;")
+        
+    def resizeEvent(self, event):
+        h = self.widget.height()
+        w = self.widget.width()
+        if w - 100 > 0:
+            self.back.setGeometry(0, 0, w, h)
