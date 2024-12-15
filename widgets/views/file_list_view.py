35fc7b09f55dc2761eeb1fef22daff0a533640e8
rpezemk <rpezemk@gmail.com>
2024-12-15 12:30:46 +0100

    almost ready audio file browser

--- widgets/views/file_list_view.py
+++ widgets/views/file_list_view.py
@@ -1,6 +1,6 @@
 import os
 from typing import Any
-from utils.file_utils.fs_model import DirModel, get_tree
+from utils.file_utils.fs_model import DirModel, ParentDirModel, get_tree
 from widgets.views.my_table_widget import MyTableView, finish_path
 
 
@@ -12,9 +12,11 @@ class FileListView(MyTableView):
         current_dir = "."
         resolved_path = os.path.abspath(current_dir)
         self.current_path = resolved_path
-
+        self.dir_model = DirModel(abs_path=self.current_path)
+        
         columns = [
-                ("rel_path", 200, True, None, self.change_dir),
+                ("d", 15, True, None, None),
+                ("rel_path", 200, True, None, self.double_click_change_dir),
                 ("ext", 20, True, None, None),
                 ("play", 20, True, self.play_click, None)
             ]
@@ -24,17 +26,25 @@ class FileListView(MyTableView):
                          on_selection_changed=self.on_selection_changed)
 
     def get_data(self):
-        self.dir_model = DirModel(abs_path=self.current_path)
         ok, self.dir_children = get_tree(self.dir_model)
-        data = [[finish_path(fs), fs.ext, fs.sel] for fs in self.dir_children]
+        data = [["D" if isinstance(fsi, DirModel) or isinstance(fsi, ParentDirModel) else "F", finish_path(fsi), fsi.ext, "PLAY"] for fsi in self.dir_children]
         return data
 
-    def play_click(self, row_data: list[Any]):
-        print("TEST CLICK PLAY")
+    def play_click(self, row_data: list[Any], row_idx):
+        if not row_data: 
+            return
+        print(f"TEST CLICK PLAY row_idx:{row_idx}")
 
-    def change_dir(self, row_data: list[Any]):
-        print("TEST DOUBLE CLICK PLAY")
-
+    def double_click_change_dir(self, row_data: list[Any], row_idx):
+        if not row_data or len(row_data) == 0: 
+            return
+        
+        fs_item = self.dir_children[row_idx]
+        if isinstance(fs_item, DirModel) or isinstance(fs_item, ParentDirModel):
+            self.dir_model = fs_item        
+        
+        self.refresh_view()
+        
     def on_selection_changed(self, selected, deselected):
             selected_rows = [index.row() for index in self.table_widget.selectedIndexes()]
             print(f"Selected rows: {selected_rows}")
--- widgets/views/my_table_widget.py
+++ widgets/views/my_table_widget.py
@@ -35,49 +35,52 @@ class MyTableView(QWidget):
         self.get_data_func = get_data_func
         self.columns = columns
             
+        
+        
+        
         self.table_widget = QTableWidget(self)
-        data = self.get_data_func()
-        self.data = data
-        n_rows = len(data)
-        n_cols = len(columns)
-        self.table_widget.setRowCount(n_rows)
-        self.table_widget.setColumnCount(n_cols)
         self.table_widget.setHorizontalHeaderLabels([c[0] for c in columns])
-        
-    
-        for idx, col_no in enumerate(columns):
-            self.table_widget.setColumnWidth(idx, col_no[1])  
-        
+        self.table_widget.setSelectionBehavior(QTableWidget.SelectRows) 
+        self.table_widget.selectionModel().selectionChanged.connect(on_selection_changed)
+        self.table_widget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.table_widget.cellClicked.connect(self.on_cell_clicked)
         self.table_widget.cellDoubleClicked.connect(self.on_cell_double_clicked)
         
+        n_cols = len(self.columns)
+        self.table_widget.setColumnCount(n_cols)
+        
+        for idx, col_no in enumerate(columns):
+            self.table_widget.setColumnWidth(idx, col_no[1])  
+            
+        self.refresh_view()
+    
+        layout = QVBoxLayout(self)
+
+        layout.addWidget(self.table_widget)
+    
+    def refresh_view(self):
+        data = self.get_data_func()
+        self.data = data
+        n_rows = len(self.data)
+        self.table_widget.setRowCount(n_rows)
+        n_cols = len(self.columns)
         for idx, row in enumerate(data):
             for col_no in range(n_cols):
                 item = QTableWidgetItem(str(row[col_no]))
                 item.setFlags(item.flags() & ~Qt.ItemIsEditable)
                 self.table_widget.setItem(idx, col_no, item)
-
-        self.table_widget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        layout = QVBoxLayout(self)
-        self.table_widget.setSelectionBehavior(QTableWidget.SelectRows) 
-        self.table_widget.selectionModel().selectionChanged.connect(on_selection_changed)
-
-        layout.addWidget(self.table_widget)
-    
-    def refresh_view(self):
-        ...
         
     def on_cell_clicked(self, row_idx, column_idx):
         click_func = self.columns[column_idx][3]
         if click_func:
             row_data = self.data[row_idx]
-            click_func(row_data)
+            click_func(row_data, row_idx)
         
     def on_cell_double_clicked(self, row_idx, column_idx):
         double_click_func = self.columns[column_idx][4]
         if double_click_func:
             row_data = self.data[row_idx]
-            double_click_func(row_data)
+            double_click_func(row_data, row_idx)
         
 
             
