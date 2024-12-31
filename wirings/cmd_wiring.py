62057ced766b9f36ea7ff98483ed903ad2cdac81
rpezemk <rpezemk@gmail.com>
2024-12-31 16:00:45 +0100

    tiny renames

--- wirings/cmd_wiring.py
+++ wirings/cmd_wiring.py
@@ -4,6 +4,7 @@ from utils.commands.kbd_resolver import KbdResolver
 from utils.logger import MLogger
 
 A = SubCmd("A", [Qt.Key_A])
+CTRL_A = SubCmd("CTRL_A", [Qt.Key_Control, Qt.Key_A])
 CTRL_T = SubCmd("CTRL_T", [Qt.Key_Control, Qt.Key_T])
 CTRL_X = SubCmd("CTRL_X", [Qt.Key_Control, Qt.Key_X])
 CTRL_Q = SubCmd("CTRL_Q", [Qt.Key_Control, Qt.Key_Q])
@@ -14,12 +15,14 @@ ARROW_LEFT = SubCmd("ARROW_LEFT", [Qt.Key_Left]) #16777236
 ARROW_RIGHT = SubCmd("ARROW_RIGHT", [Qt.Key_Right]) 
 ARROW_UP = SubCmd("ARROW_UP", [Qt.Key_Up])
 ARROW_DOWN = SubCmd("ARROW_DOWN", [Qt.Key_Down])
-# ALL = CompoundCommand("ALL", [A], lambda: print("A-ALL")),
-# ALL = CompoundCommand("RENAME", [CTRL_R, CTRL_R], lambda: print("RENAME")),
-# ALL = CompoundCommand("DELETE", [CTRL_T, CTRL_X], lambda: print("DELETE")),
-# ALL = CompoundCommand("CLOSE", [CTRL_T, CTRL_Q], lambda: print("CLOSE")),
-# ALL = CompoundCommand("SPACE", [SPACE], lambda: print("SPACE")),
-# ALL = CompoundCommand("RETURN", [RETURN], lambda: print("RETURN")),
+
+SELECT_ALL = CompoundCommand("SELECT_ALL", [CTRL_A], lambda: print("A-ALL")),
+RENAME = CompoundCommand("RENAME", [CTRL_R, CTRL_R], lambda: print("RENAME")),
+DELETE = CompoundCommand("DELETE", [CTRL_T, CTRL_X], lambda: print("DELETE")),
+PLAY = CompoundCommand("PLAY", [SPACE], lambda: print("SPACE")),
+PLAY_SPECIAL = CompoundCommand("PLAY_SPECIAL", [RETURN], lambda: print("RETURN")),
+
+
 PREV = CompoundCommand("ARROW_LEFT", [ARROW_LEFT])
 NEXT = CompoundCommand("ARROW_RIGHT", [ARROW_RIGHT])
 UP = CompoundCommand("ARROW_UP", [ARROW_UP])
